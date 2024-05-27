ceddf675fe50ec809e3fcc0dac2aaebc0097c1d1
Vishesh Gupta <guptavishesh264@gmail.com>
2024-05-27 02:06:09 -0400

    fastapi training pipeline fixed

---
+++ pipeline/mlflow/mlflow_tracking.py
@@ -0,0 +1,22 @@
+import os
+from random import random, randint
+
+import mlflow
+
+if __name__ == "__main__":
+    with mlflow.start_run() as run:
+        mlflow.set_tracking_uri("http://mlflow:5000")
+        print("Running mlflow_tracking.py")
+
+        mlflow.log_param("param1", randint(0, 100))
+
+        mlflow.log_metric("foo", random())
+        mlflow.log_metric("foo", random() + 1)
+        mlflow.log_metric("foo", random() + 2)
+
+        # if not os.path.exists("outputs"):
+        #     os.makedirs("outputs")
+        # with open("outputs/test.txt", "w") as f:
+        #     f.write("hello world!")
+
+        # mlflow.log_artifacts("outputs")
